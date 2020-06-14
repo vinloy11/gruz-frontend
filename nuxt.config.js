9b71b701d158d45d7fc6cbcb457d2410b0e70977
Shtork A.O <shtork@prodv.net>
2020-06-14 11:47:48 +0700

    #1 Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'gruz',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Trucking industry' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  buildModules: [
+    // Simple usage
+    '@nuxtjs/vuetify',
+    '@nuxtjs/pwa',
+  ],
+
+  server: {
+    port: 8080, // default: 3000
+  },
+
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
